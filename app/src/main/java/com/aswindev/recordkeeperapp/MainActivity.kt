4d9398e00413a19db0729c5a2a548925db71b6a1
aswinthomas <aswinthomas@gmail.com>
2023-12-26 21:41:14 +0800

    add onitemselectedlistener

--- app/src/main/java/com/aswindev/recordkeeperapp/MainActivity.kt
+++ app/src/main/java/com/aswindev/recordkeeperapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.aswindev.recordkeeperapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import androidx.fragment.app.commit
 import com.aswindev.recordkeeperapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,11 +16,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    private fun onRunningClicked() {
         supportFragmentManager.commit {
             // cant use binding here since ID is needed
-            add(R.id.frame_content, RunningFragment())
+            replace(R.id.frame_content, RunningFragment())
         }
-
-
     }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            // cant use binding here since ID is needed
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            return true
+        } else if (item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            return true
+        }
+        return false
+    }
+
 }
+
